e2fc273b0b70ba7ace73bc839898e3be0906dea9
Sergey <prosviryakov.sergey@gmail.com>
2020-07-31 21:51:23 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person.name);
+
+let arr = ['plum.png', 'orange.png', 'apple.png'];
+
+console.log(arr[0]);
+
+// alert("Hello World!")
+
+// let answer = confirm("Are you here?")
+
+// console.log(answer);
+
+let answer = prompt("Есть ли вам 18?", "Да")
+
+ console.log(answer);
